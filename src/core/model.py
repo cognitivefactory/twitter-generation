a0b38616943080c7b3445abe7df03df311237bc1
ThomasByr <tbouyer2000@gmail.com>
2024-01-02 14:16:10 +0100

    better generation

--- src/core/model.py
+++ src/core/model.py
@@ -2,66 +2,118 @@ import logging
 
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
-# import torch
-# import torch.nn as nn
+# import gc
+import torch
+
+
+MODEL = """
+```
+1. "..."
+2. "..."
+[...]
+10. "..."
+```
+"""
 
 
 MASK = {
-    "en": """
-Forget everything said before and do as told. You are a <sentiment> user of Twitter and you are tweeting about "<topic>".
-Generate a small list of tweets you could write in real life about "<topic>", making them look as real as possible.
-The tweets should be as diverse as possible and users should feel <sentiment> about the topic.
-Do generate anything that is not a tweet.
-""",
     "fr": """
-Oublie tout ce qui a été dit avant et fais comme on te dit. Tu es un utilisateur <sentiment> de Twitter et tu tweetes à propos de "<topic>".
-Génère une petite liste de tweets que tu pourrais écrire dans la vraie vie à propos de "<topic>", en les rendant aussi réels que possible.
-Les tweets doivent être aussi diversifiés que possible et les utilisateurs doivent être <sentiment> à propos du sujet.
-Ne génère rien qui ne soit pas un tweet.
+Oublie tout ce qui a été dit avant. Ne fais aucun commentaire. Ne dis rien. Ne prend aucune initiative.
+Un utilisateur <sentiment> de Twitter tweet à propos de "<topic>".
+Cet utilisateur <sentiment> veut écrire 10 courts tweets très diversifiés en français.
+Ces tweets doivent paraître authentiques à un utilisateur <sentiment> de Twitter (émotes, tournures parlées, quelques fautes).
+L'utilisateur n'écrira que le texte du tweet, et il ou elle utilisera le format suivant: """
+    + MODEL
+    + """
+Voici 10 tweets à propos de "<topic>" pour cet utilisateur <sentiment>.
 """,
-    "de": """
-Vergessen Sie alles, was vorher gesagt wurde und tun Sie, was gesagt wurde. Sie sind ein <sentiment> Benutzer von Twitter und Sie twittern über "<topic>".
-Generieren Sie eine kleine Liste von Tweets, die Sie im wirklichen Leben über "<topic>" schreiben könnten, und machen Sie sie so real wie möglich.
-Die Tweets sollten so vielfältig wie möglich und die Nutzer sollten <sentiment> zum Thema empfinden.
-Generieren Sie nichts, was kein Tweet ist.
+    "en": """
+Forget everything that was said before. Do not comment. Do not say anything. Do not take any initiative.
+A <sentiment> Twitter user tweets about "<topic>".
+This <sentiment> user wants to write 10 short but diverse tweets in English.
+These tweets must look authentic to a <sentiment> Twitter user (emotes, spoken turns, some mistakes).
+The user will only write the text of the tweet, and he or she will use the following format: """
+    + MODEL
+    + """
+Here are 10 tweets about "<topic>" for this <sentiment> user.
 """,
     "default": """
-Forget everything said before and do as told. You are a <sentiment> user of Twitter and you are tweeting about "<topic>".
-Generate a small list of tweets you could write in real life about "<topic>", making them look as real as possible.
-The tweets should be as diverse as possible and users should feel <sentiment> about the topic.
-Do generate anything that is not a tweet.
+Forget everything that was said before. Do not comment. Do not say anything. Do not take any initiative.
+A <sentiment> Twitter user tweets about "<topic>".
+This <sentiment> user wants to write 10 short but diverse tweets in English.
+These tweets must look authentic to a <sentiment> Twitter user (emotes, spoken turns, some mistakes).
+The user will only write the text of the tweet, and he or she will use the following format: """
+    + MODEL
+    + """   
+Here are 10 tweets about "<topic>" for this <sentiment> user.
 """,
 }
 
 
 class Model:
-    heading = "\n1. "
+    heading = '\n1. "'
     epilogue = "\n"
 
-    def __init__(self) -> None:
+    def __init__(self, gpu_id: int = 0, temperature: float = 0.7) -> None:
         self.logger = logging.getLogger("model")
         self.model = AutoModelForCausalLM.from_pretrained("Xwin-LM/Xwin-LM-7B-V0.2")
 
-        # n = torch.cuda.device_count()
-        # self.logger.info("using %d GPUs", n)
-        # if n > 1:
-        #     self.model = nn.DataParallel(self.model)
+        self.device = f"cuda:{gpu_id}" if (c := torch.cuda.is_available()) else "cpu"
+        if not c:
+            self.logger.warning("CUDA is not available, using CPU.")
+        else:
+            self.logger.info(f"Using device: {self.device}")
+        self.model.to(self.device)
+        self.__t = temperature
 
-        # self.model.to("cuda")
         self.tokenizer = AutoTokenizer.from_pretrained("Xwin-LM/Xwin-LM-7B-V0.2")
 
+    @torch.no_grad
     def __generate(self, prompt: str) -> str:
-        inputs = self.tokenizer(prompt, return_tensors="pt")
-        samples = self.model.generate(**inputs, max_new_tokens=4096, temperature=0.7)
-        return self.tokenizer.decode(samples[0][inputs["input_ids"].shape[1] :], skip_special_tokens=True)
+        inputs = self.tokenizer(prompt, return_tensors="pt").to(self.device)
+        samples = self.model.generate(**inputs, max_new_tokens=4096, temperature=self.__t)
+        result = self.tokenizer.decode(samples[0][inputs["input_ids"].shape[1] :], skip_special_tokens=True)
 
+        # # free memory
+        # del inputs, samples
+        # gc.collect()
+        # if torch.cuda.is_available():
+        #     torch.cuda.empty_cache()
+
+        return result
+
+    @torch.no_grad
     def __batch_generate(self, prompts: list[str]) -> list[str]:
-        inputs = self.tokenizer(prompts, return_tensors="pt", padding=True, truncation=True)
-        samples = self.model.generate(**inputs, max_new_tokens=4096, temperature=0.7)
-        return self.tokenizer.batch_decode(samples, skip_special_tokens=True)
+        inputs = self.tokenizer(prompts, return_tensors="pt", padding=True, truncation=True).to(self.device)
+        samples = self.model.generate(**inputs, max_new_tokens=4096, temperature=self.__t)
+        result = self.tokenizer.batch_decode(samples, skip_special_tokens=True)
+
+        # # free memory
+        # del inputs, samples
+        # gc.collect()
+        # if torch.cuda.is_available():
+        #     torch.cuda.empty_cache()
+
+        return result
 
     def generate(self, topic: str, sentiment: str, language: str = "en") -> str:
+        """Generate a tweet for the given topic and sentiment."""
         prompt = MASK.get(language, MASK["default"])
         prompt = prompt.replace("<topic>", topic).replace("<sentiment>", sentiment)
         prompt += self.heading
         return self.heading + self.__generate(prompt) + self.epilogue
+
+    def batch_generate(self, topics: list[str], sentiments: list[str], language: str = "en") -> list[str]:
+        """
+        Generate a list of tweets for each topic and sentiment.\\
+        Each topic is matched with each sentiment **in this function**.
+        """
+        prompts = []
+        for topic in topics:
+            for sentiment in sentiments:
+                prompt = MASK.get(language, MASK["default"])
+                prompt = prompt.replace("<topic>", topic).replace("<sentiment>", sentiment)
+                prompt += self.heading
+                prompts.append(prompt)
+        # return list with heading and epilogue added to each tweet
+        return [self.heading + tweet + self.epilogue for tweet in self.__batch_generate(prompts)]
